c6433698bebaa362a028547a98c415b95413ccf3
haneulk <haneulk@gmail.com>
2026-09-14 07:07:48 +0900

    fix: use canonical research cache identity

--- api/research.js
+++ api/research.js
@@ -1,4 +1,10 @@
 import { createResearchHandler } from '../lib/price-research.js';
 import { createPromptAwareFetch } from '../lib/resell-grounding.js';
+import { parseAndCanonicalizeResearchBody } from '../lib/research-request.js';
 
-export default createResearchHandler({ fetchImpl: createPromptAwareFetch() });
+const researchHandler = createResearchHandler({ fetchImpl: createPromptAwareFetch() });
+
+export default async function handler(req, res) {
+  req.body = parseAndCanonicalizeResearchBody(req.body);
+  return researchHandler(req, res);
+}
