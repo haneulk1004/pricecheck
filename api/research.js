8876653414c7451021c2b5b7403b840ea8188f7b
haneulk <haneulk@gmail.com>
2026-09-14 07:32:30 +0900

    fix: remove duplicate source links from research responses

--- api/research.js
+++ api/research.js
@@ -1,10 +1,15 @@
 import { createResearchHandler } from '../lib/price-research.js';
 import { createPromptAwareFetch } from '../lib/resell-grounding.js';
 import { parseAndCanonicalizeResearchBody } from '../lib/research-request.js';
+import { dedupeResearchPayload } from '../lib/source-dedupe.js';
 
 const researchHandler = createResearchHandler({ fetchImpl: createPromptAwareFetch() });
 
 export default async function handler(req, res) {
   req.body = parseAndCanonicalizeResearchBody(req.body);
+
+  const originalJson = res.json.bind(res);
+  res.json = data => originalJson(dedupeResearchPayload(data));
+
   return researchHandler(req, res);
 }
