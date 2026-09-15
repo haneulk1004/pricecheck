393dac087acbb1b061bf867315346254e04f6ef0
haneulk <haneulk@gmail.com>
2026-09-15 18:02:34 +0900

    chore: mirror failing research input in self-test

--- api/selftest-research.js
+++ api/selftest-research.js
@@ -4,7 +4,7 @@ export default async function handler(req, res) {
   if (req.method !== 'GET') return res.status(405).json({ code: 'METHOD_NOT_ALLOWED' });
   const syntheticReq = {
     method: 'POST',
-    body: { mode: 'store', brand: 'Logitech', productName: 'MX Keys Mini', modelCode: '', size: '' },
+    body: { mode: 'store', brand: '로지텍', productName: 'MX Keys Mini', modelCode: '', size: '' },
     headers: req.headers,
     socket: req.socket
   };
